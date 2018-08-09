7891b9c8ed4f3b7ba8ea586f7b8105a4c38a7a5e
Raphael Fonseca <kerberuz@gmail.com>
2018-08-09 18:08:31 -0300

    python script collect twitter data debate

---
+++ 2018-brazil-elections/debate-01-band/debateBandPresidencia20180809.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import oauth2 as oauth
+import json
+import time
+import pymongo
+
+CONSUMER_KEY = "U5pSmz0NoYTzfyz5tpIm002vG"
+CONSUMER_SECRET = "YU8COPGUfkgpta3p2eDZ1HbLpryrskTLExpBPdpNkDqOTPAyGQ"
+ACCESS_KEY = "67772843-7Twvn0qPExs646xSg0nxEakgCa6OPHrz0V3ETt24i"
+ACCESS_SECRET = "jKPsSGgmV1cNZ5GQSRLAJ2wpFVm9luaVHWjWn7m2FfLKB"
+
+consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+client = oauth.Client(consumer, access_token)
+
+clientMongo = pymongo.MongoClient("localhost", 27017)
+db = clientMongo.db_cores_observatory
+
+since_id = '' #pode usar since_id ou nao, se usar, colocar na url
+
+#Geolocalização da Central do Brasil
+#Latitude, Longitude, Raio Radius ao redor da posição.
+#Com este radius de 70km, a área abrange toda Região Metropolitana do RJ.
+geo='-15.8000555,-47.8620834,3000km'
+
+#Desde. Formato YYYY-MM-DD.
+since='2018-08-09'
+
+#Até o dia anterior de. Formato YYYY-MM-DD.
+until='2018-08-10'
+
+#Lista de Termos, separe por vírgulas.
+
+query = ['']
+
+for q in query:
+
+        max_id = '0'
+        contadorTweets = 0
+        continua = 1
+
+        while(continua == 1):
+                try:
+
+                        if(max_id == '0'):
+
+                                URL = "https://api.twitter.com/1.1/search/tweets.json?geocode="+geo+"&since="+since+"&until="+until+"&count=100"+"&lang=pt"
+                        else:
+
+                                URL = "https://api.twitter.com/1.1/search/tweets.json?geocode="+geo+"&since="+since+"&until="+until+"&count=100"+"&lang=pt"+"&max_id="+str(max_id)
+                        max_id_ant = max_id
+                        response, data = client.request(URL, "GET")
+                        debateBandPresidencia20180809 = json.loads(data)
+                        for tweet in debateBandPresidencia20180809['statuses']:
+                                                        
+                                db.debateBandPresidencia20180809.update({'id': tweet['id']},tweet, upsert=True)
+                                contadorTweets = contadorTweets + 1
+                                tweet['text']==dict
+                                tx = tweet['text']
+                                print "Termo Buscado: "+ q
+                                print "\n"
+                                print str("Tweet: " + tx.encode('utf-8'))
+                                print "\n"
+                                print "Número de tweets com termo atual: "
+                                print contadorTweets
+                                print "\n"
+                                print "ID do Tweet: "
+                                print max_id
+                                print '======================================================'
+                                max_id = tweet['id'] - 1
+                                
+                        time.sleep(2)
+                        if(max_id == max_id_ant):                                
+                                continua = 0
+                        
+                except Exception, e:
+
+                        print e
+                        print 'dormiu'
+                        time.sleep(15*60)
+                        pass
